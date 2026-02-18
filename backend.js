cdeef13448df99589b7c8ace2f54bdf32207b14a
Lulcas01 <l.abeats2238@gmail.com>
2026-02-18 15:46:26 -0300

    aljlh

--- backend.js
+++ backend.js
@@ -60,7 +60,7 @@ const Transferencia = mongoose.model("Transferencia", transferenciaSchema);
 
 
 app.use(cors({
-    origin: ['http://localhost:5173', process.env.CLIENT_URL,'http://localhost:3000'],
+    origin: ['http://localhost:5173', process.env.CLIENT_URL,'http://localhost:3000',process.env.CLIENT_URL2],
     credentials: true // Permite o tráfego de cookies
 }));
 
